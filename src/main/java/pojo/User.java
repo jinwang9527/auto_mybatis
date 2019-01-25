462d013a3d83d734dae7e401ea600e12ccc5990e
zhangsan <838210051@qq.com>
2019-01-25 14:35:07 +0800

    first commit

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,45 @@
+package pojo;
+
+import java.util.Date;
+
+public class User {
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Boolean isDel;
+
+    private String name;
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Boolean getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Boolean isDel) {
+        this.isDel = isDel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
